3f482014efdc788a6a13408d9b4494d03f330853
Marian Schubert <marian.schubert@gmail.com>
2018-08-14 11:14:08 +0200

    Empty extension

---
+++ Demo/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Demo
+//
+//  Created by Marián Schubert on 14/08/2018.
+//  Copyright © 2018 Marián Schubert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MyExtension/SafariExtensionViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SafariExtensionViewController.swift
+//  MyExtension
+//
+//  Created by Marián Schubert on 14/08/2018.
+//  Copyright © 2018 Marián Schubert. All rights reserved.
+//
+
+import SafariServices
+
+class SafariExtensionViewController: SFSafariExtensionViewController {
+    
+    static let shared = SafariExtensionViewController()
+
+}
